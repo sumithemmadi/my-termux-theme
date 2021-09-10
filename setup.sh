ed7417aa3979f1c0e1bfbaab216f3e39f2efef7c
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-09-11 00:05:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 mkdir .termux
 cp -vrf $HOME/my-termux-theme/* $HOME/.termux
 termux-reload-settings
-cp -vrf $HOME/.bashrc $HOME/.bashrc.back
-echo "Createing .bashrc.back file for .bashrc"
-cp -vrf $HOME/my-termux-theme/.bashrc $HOME
+
+FILE=$HOME/.bashrc.back
+if [ -f "$FILE" ]; then
+    echo "$FILE exist."
+    echo "\033[0;31mBackup already saved as  \033[0;32m.bashrc.back\n"
+
+else 
+    echo "$FILE does not exist."
+    cp -vrf $HOME/.bashrc $HOME/.bashrc.back
+fi
+printf "\n\n\033[0;31mCreated \033[0;32m.bashrc.back \033[0;31mfile for \033[0;32m.bashrc\n"
+sleep 5
+printf "\n\033[0;31mCreating \033[0;32m.bashrc"
+cp -rf $HOME/my-termux-theme/.bashrc $HOME
 clear
-printf "\033[0;31mCreated \033[0;32m.bashrc.back \033[0;31mfile for \033[0;32m.bashrc\n"
-printf "\033[0;32m Enter '\033[0;31mexit\033[0;32m' command and close all tabs and reopen termux again\n"
+neofetch
+printf "\n\033[0;31mCreated \033[0;32m.bashrc.back \033[0;31mfile for \033[0;32m.bashrc\n"
+printf "\n\033[0;32m Enter '\033[0;31mexit\033[0;32m' command and close all tabs and reopen termux again\n"
 sleep 5
