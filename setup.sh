c584c3826db804e49c22cd768161b9e6e9259c8e
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-09-10 23:12:22 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+rm -v $HOME/.termux
+cp -vrf $HOME/my-termux-theme/* $HOME.termux
+termux-reload-settings
+sleep 2
+echo -v "clear" >> $PREFIX/etc/bash.bashrc
+echo -v "neofetch" >> $PREFIX/etc/bash.bashrc
+exit
