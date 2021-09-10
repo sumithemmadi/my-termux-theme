6a7250b00dad025004ccb87dd3569772c405383d
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-09-10 23:52:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,5 @@ echo "Createing .bashrc.back file for .bashrc"
 cp -vrf $HOME/my-termux-theme/.bashrc $HOME
 clear
 printf "\033[0;31mCreated \033[0;32m.bashrc.back \033[0;31mfile for \033[0;32m.bashrc\n"
+printf "\033[0;32m Enter '\033[0;31mexit\033[0;32m' command and close all tabs and reopen termux again\n"
 sleep 5
