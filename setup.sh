5792df02674f7608a485c7f4ff276db413603b76
Emmadi Sumith Kumar <sumithemmadi244@gmail.com>
2021-09-10 23:32:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+mkdir .termux
 cp -vrf $HOME/my-termux-theme/* $HOME/.termux
 termux-reload-settings
+cp -vrf $HOME/.bashrc $HOME/.bashrc.back
+echo "Createing .bashrc.back file for .bashrc"
+sleep 5
+cp -vrf $HOME/my-termux-theme/.bashrc $HOME
 sleep 2
-exit
